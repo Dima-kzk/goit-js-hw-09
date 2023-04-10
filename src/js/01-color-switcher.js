a786a5e9559db3461f46ed9e87540884749d8a71
Dima-kzk <dimakzk@yahoo.com>
2023-04-10 23:43:35 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function startChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', '');
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+}
+
+stopBtn.setAttribute('disabled', '');
+
+startBtn.addEventListener('click', startChangeColor);
+
+stopBtn.addEventListener('click', stopChangeColor);
